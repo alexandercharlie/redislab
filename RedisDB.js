deae5df6f17916f72fd6bcddb06e70f2cbd9d809
Alex <alex.charlie.young@live.in>
2017-08-08 23:49:53 +0530

    Update RedisDB.js

--- RedisDB.js
+++ RedisDB.js
@@ -1,7 +1,7 @@
 var redis = require('redis');
-var port=10427;
-var host='redis-10427.c10.us-east-1-2.ec2.cloud.redislabs.com';
-var pass='@alex#441';
+var port=10111;//Your Redis cloud PORT
+var host='redis-10111.c10.us-west-1-4.ec2.cloud.redislabs.com';//Your Redis cloud host name
+var pass='#####';//Redis cloud password
 var client = redis.createClient(port,host, {no_ready_check: true});
 client.auth(pass, function (err) {
     if (err) throw err;
